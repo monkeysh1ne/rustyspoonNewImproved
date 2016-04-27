8c5e1abc43911ee61c67013e0da0c603a01a7758
monkeysh1ne <mcdoob@gmail.com>
2016-04-28 07:34:45 +1200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp'),
+	browserSync = require('browser-sync'),
+	sass = require('gulp-sass'),
+	pug = require('gulp-pug'),
+	reload = browserSync.reload;
+
+
+/**
+ * compile pug files into html
+ */
+ gulp.task('templates', function() {
+ 	
+ 	var YOUR_LOCALS = {};
+
+ 	return gulp.src('./app/*.pug')
+ 		.pipe(pug({
+ 			locals: YOUR_LOCALS
+ 		}))
+ 		.pipe(gulp.dest('./dist/'))
+ });
+
+ /**
+  * Important!!
+  * Separate task for the reaction to '.pug' files
+  */
+ gulp.task('pug-watch', ['templates'], reload);
+
+ /**
+  * Sass task for live injection into all browsers
+  */
+ gulp.task('sass', function(){
+ 	return gulp.src('./app/sass/*.sass')
+ 		.pipe(sass())
+ 		.pipe(gulp.dest('./dist/css'))
+ 		.pipe(reload({ stream: true }));
+ });
+
+gulp.task('pug', function() {
+	return gulp.src('src/**/*.pug')
+		.pipe(pug())
+		.pipe(gulp.dest('builds/development'));
+});
+
+/**
+ * Serve and watch the sass/pug files for changes
+ */
+ gulp.task('default', ['sass', 'templates'], function() {
+ 	browserSync({server: './dist'});
+
+ 	gulp.watch('./app/sass/*.sass', ['sass']);
+ 	gulp.watch('./app/*.pug', ['pug-watch']);
+ });
+
